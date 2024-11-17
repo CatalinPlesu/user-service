a8dfe02fa41aeee6e978599a4da00dfcee0b64dd
Catalin Plesu <catalinhimself@gmail.com>
2024-11-17 10:45:25 +0200

    fix: dont duplicate jwts

--- repository/jwts/redis.go
+++ repository/jwts/redis.go
@@ -42,6 +42,13 @@ func (r *RedisRepo) Insert(ctx context.Context, userID uuid.UUID, jwt string) er
 		if err != nil {
 			return fmt.Errorf("failed to decode user JWTs json: %w", err)
 		}
+
+		for _, existingJWT := range userJWTs.JWTs {
+			if existingJWT == jwt {
+				return nil
+			}
+		}
+
 		userJWTs.JWTs = append(userJWTs.JWTs, jwt)
 	}
 
@@ -53,67 +60,6 @@ func (r *RedisRepo) Insert(ctx context.Context, userID uuid.UUID, jwt string) er
 	err = r.Client.Set(ctx, key, string(data), 0).Err()
 	if err != nil {
 		return fmt.Errorf("failed to set user JWTs: %w", err)
-	}
-
-	return nil
-}
-
-func (r *RedisRepo) FindByID(ctx context.Context, userID uuid.UUID) (model.UserJWTs, error) {
-	key := userJWTsKey(userID)
-
-	value, err := r.Client.Get(ctx, key).Result()
-	if errors.Is(err, redis.Nil) {
-		return model.UserJWTs{}, ErrNotExist
-	} else if err != nil {
-		return model.UserJWTs{}, fmt.Errorf("failed to get user JWTs: %w", err)
-	}
-
-	var userJWTs model.UserJWTs
-	err = json.Unmarshal([]byte(value), &userJWTs)
-	if err != nil {
-		return model.UserJWTs{}, fmt.Errorf("failed to decode user JWTs json: %w", err)
-	}
-
-	return userJWTs, nil
-}
-
-func (r *RedisRepo) DeleteJWT(ctx context.Context, userID uuid.UUID, jwt string) error {
-	key := userJWTsKey(userID)
-
-	var userJWTs model.UserJWTs
-	value, err := r.Client.Get(ctx, key).Result()
-	if errors.Is(err, redis.Nil) {
-		return ErrNotExist
-	} else if err != nil {
-		return fmt.Errorf("failed to get user JWTs: %w", err)
-	}
-
-	err = json.Unmarshal([]byte(value), &userJWTs)
-	if err != nil {
-		return fmt.Errorf("failed to decode user JWTs json: %w", err)
-	}
-
-	found := false
-	for i, existingJWT := range userJWTs.JWTs {
-		if existingJWT == jwt {
-			userJWTs.JWTs = append(userJWTs.JWTs[:i], userJWTs.JWTs[i+1:]...)
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		return ErrJWTNotFound
-	}
-
-	data, err := json.Marshal(userJWTs)
-	if err != nil {
-		return fmt.Errorf("failed to encode user JWTs: %w", err)
-	}
-
-	err = r.Client.Set(ctx, key, string(data), 0).Err()
-	if err != nil {
-		return fmt.Errorf("failed to set updated user JWTs: %w", err)
 	}
 
 	return nil
